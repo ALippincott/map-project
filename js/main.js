ca79e9d1edb9e55396b3534ec142b76290357684
ALippincott <alex.lippincott19@gmail.com>
2017-08-03 17:12:08 -0400

    End of the day

--- js/main.js
+++ js/main.js
@@ -1,70 +1,91 @@
+function Place(locationList) {
+		this.name = locationList.name;
+		this.latLng = locationList.latLng;
+		this.marker = null;
+	}
+var googleMap;
+var locations = [];
+var locationList = [
+	{name: 'Basil Thai', latLng: {lat: 40.422848654474635,lng: -86.90782070159912}},
+	{name: 'Chipotle',latLng: {lat: 40.42362457637239,lng: -86.9071501493454}},
+	{name: 'Blue Nile',latLng: {lat: 40.4245270847936,lng: -86.90841615200043}},
+	{name: 'Maru',latLng: {lat: 40.42450258246246,lng: -86.90679609775543}}, 
+	{name: 'Town and Gown',latLng: {lat: 40.42286090594257,lng: -86.90416753292084}}, 
+	{name: 'Oishi',latLng: {lat: 40.42146014031933,lng: -86.90384566783905}}
+	];
 
-
+locationList.forEach(function(place) {
+		locations.push(new Place(place));
+	});
+var marker = new google.maps.Marker({
+            map: map,
+            position: place.latLng,
+            name: place.name ,
+            animation: google.maps.Animation.DROP,
+            id: i
+          });
+function populateInfoWindow(marker, infowindow) {
+        // Check to make sure the infowindow is not already opened on this marker.
+        if (infowindow.marker != marker) {
+          infowindow.marker = marker;
+          infowindow.setContent('<div>' + marker.name + '</div>');
+          infowindow.open(map, marker);
+          // Make sure the marker property is cleared if the infowindow is closed.
+          infowindow.addListener('closeclick',function(){
+            infowindow.setMarker = null;
+          });
+      }
+  }
 //View Model
 var koViewModel = function(map, locationList) {
 	var self = this;
-	self.googleMap = map;
-	self.allPlaces = [];
-	//Observable Array of locations
-	self.locations = ko.observableArray([
-	{name: 'Basil Thai', latLng: {lat: 40.422848654474635,lng: -86.90782070159912}},
-	{name: 'Chipotle', latLng: {lat: 40.42362457637239,lng: -86.9071501493454}},
-	{name: 'Blue Nile', latLng: {lat: 40.4245270847936,lng: -86.90841615200043}},
-	{name: 'Maru', latLng: {lat: 40.42450258246246,lng: -86.90679609775543}}, 
-	{name: 'Town and Gown', latLng: {lat: 40.42286090594257,lng: -86.90416753292084}}, 
-	{name: 'Oishi', latLng: {lat: 40.42146014031933,lng: -86.90384566783905}}
-	]);
-	//pushes location into allPlaces array
-	locationList.forEach(function(place) {
-		self.allPlaces.push(new Place(place));
-	});
-	//Creates markers and infowindows for every place in allPlaces
-	self.allPlaces.forEach(function(place) {
-		
-		var markerOptions = {
-			map: self.googleMap,
-			position: place.latLng,
-			animation: google.maps.Animation.DROP,
-		};
-		var infowindow = new google.maps.InfoWindow({
-			content: place.name
-		});
-		place.marker = new google.maps.Marker(markerOptions);
-		place.marker.addListener('click', function() {
-			infowindow.open(map, place.marker);
-		
-		});
-		
-	});
-	
+	self.allPlaces = locations;	
 	//Creates seperate visible array for list
 	 self.visiblePlaces = ko.observableArray();
 	 self.allPlaces.forEach(function(place) {
 	 	self.visiblePlaces.push(place);
 	 });
-	 //Takes in input and handles filtering
-	 self.userInput = ko.observable('');
-	 self.filterMarkers = function() {
-	 	var searchInput = self.userInput().toLowerCase();
-	 	self.visiblePlaces.removeAll();
-	 	self.allPlaces.forEach(function(place) {
-	 		place.marker.setMap(null);
-		if (place.name.toLowerCase().indexOf(searchInput) !== -1) {
-				self.visiblePlaces.push(place);
-			}
+	 var markerOptions;
+	 var infowindow = [];
+	 var markers = [];
+	 var largeInfowindow = new google.maps.InfoWindow();
+	 self.visiblePlaces().forEach(function(place){
+	 	markerOptions = {
+			map: googleMap,
+			position: place.latLng,
+			animation: google.maps.Animation.DROP
+			};
+			place.marker = new google.maps.Marker(markerOptions);
+			markers.push(place.marker);
+			infowindow = new google.maps.InfoWindow({
+				content: place.name
+
+			});
+			place.marker.addListener('click', function() {
+				populateInfoWindow(this, largeInfowindow);
+			});
+			console.log(place.name);
 		});
-		//pushes markers onto map for visible places
-		self.visiblePlaces().forEach(function(place) {
-		place.marker.setMap(self.googleMap);
-	 	});
+		
+	
+
+	 //Takes in input and handles filtering
+	 // self.userInput = ko.observable('');
+	 // self.filterMarkers = function() {
+	 // 	var searchInput = self.userInput().toLowerCase();
+	 // 	self.visiblePlaces.removeAll();
+	 // 	self.allPlaces.forEach(function(place) {
+	 // 		place.marker.setMap();
+		// if (place.name.toLowerCase().indexOf(searchInput) !== -1) {
+		// 		self.visiblePlaces.push(place);
+		// 	}
+		// });
+		
+		
 	};
-	// model for place
-	function Place(locationList) {
-		this.name = locationList.name;
-		this.latLng = locationList.latLng;
-		this.marker = null;
-	}
-};
+	
+
+
 //Creates Map
 function initMap() {
 	return new google.maps.Map(document.getElementById('map'), {
@@ -78,16 +99,8 @@ function initMap() {
 
 //calls map creation when window is loaded and applys bindings
 google.maps.event.addDomListener(window, 'load', function() {
-	var locationList = [
-	{name: 'Basil Thai', latLng: {lat: 40.422848654474635,lng: -86.90782070159912}},
-	{name: 'Chipotle',latLng: {lat: 40.42362457637239,lng: -86.9071501493454}},
-	{name: 'Blue Nile',latLng: {lat: 40.4245270847936,lng: -86.90841615200043}},
-	{name: 'Maru',latLng: {lat: 40.42450258246246,lng: -86.90679609775543}}, 
-	{name: 'Town and Gown',latLng: {lat: 40.42286090594257,lng: -86.90416753292084}}, 
-	{name: 'Oishi',latLng: {lat: 40.42146014031933,lng: -86.90384566783905}}
-	];
-
-	var googleMap = initMap();
+	
+	googleMap = initMap();
 	ko.applyBindings( new koViewModel(googleMap, locationList));
 
 	
